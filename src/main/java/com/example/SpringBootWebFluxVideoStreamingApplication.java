3469cf280406f14b405856ab3584118462ccf96f
TejasreeKoneti <tejasree.k@smartgig.tech>
2024-02-09 11:39:28 +0530

    video streaming

---
+++ src/main/java/com/example/SpringBootWebFluxVideoStreamingApplication.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+@RestController
+public class SpringBootWebFluxVideoStreamingApplication {
+
+    @Autowired
+    private StreamingService streamingService;
+
+    @GetMapping(value = "videos/{title}", produces = "video/mp4")
+    public Mono<Resource> getVideos(@PathVariable String title, @RequestHeader("Range") String range) {
+        System.out.println("range in bytes() : " + range);
+        return streamingService.getVideos(title);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebFluxVideoStreamingApplication.class, args);
+    }
+
+}
